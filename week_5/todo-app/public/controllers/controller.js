e270a6789ff0812fac0aa676182deda8c585995f
marty <marty@martypdx.com>
2015-11-30 12:45:56 -0800

    create tasksServices and show local and server impl

--- week_5/todo-app/public/controllers/controller.js
+++ week_5/todo-app/public/controllers/controller.js
@@ -2,15 +2,51 @@ var Todo = angular.module('Todo', [
 	'todo.moment-filter']
 );
 
-Todo.controller('TaskCtrl', ['$scope', TaskCtrl]);
+var local = [ '$q', function local($q) {
+    return {
+        get: function(cb) {
+            var tasks = $q.defer();
+            tasks.resolve([
+                {
+                    description: 'foo local task description - task 1',
+                    done: false,
+                    created: new Date()
+                },
+                {
+                    description: 'bar local task description - task 2',
+                    done: true,
+                    created: new Date()
+                }
+            ]);
 
-function TaskCtrl($scope) {
-    $scope.tasks = [
-        {
-            description: 'some task description - task 1'
-        },
-        {
-            description: 'some task description - task 2'
+            return tasks.promise;
         }
-    ]
+    };
+}];
+
+var server = [ '$http', function($http) {
+    return {
+        get: function(cb) {
+            return $http.get('tasks').then( function(result) {
+                return result.data;
+            });
+        }
+    };
+}];
+
+Todo.factory( 'tasksService', server);
+
+Todo.controller('TaskCtrl', ['$scope', 'tasksService', TaskCtrl]);
+// Todo.controller('TaskCtrl', ['$scope', '$http', TaskCtrl]);
+
+function TaskCtrl($scope, tasksService) {
+    tasksService.get().then( function(tasks){
+        $scope.tasks = tasks;
+    });
+
+    $scope.search = {};
+    $scope.showSearch = function(){
+    	console.log($scope);
+    	$scope.search.$ = 'haha';
+    }
 }
--- week_5/todo-app/server.js
+++ week_5/todo-app/server.js
@@ -7,6 +7,21 @@ app.get('/', function (req, res) {
     res.sendFile('./public/index.html');
 });
 
+app.get('/tasks', function (req, res) {
+	res.json([
+        {
+            description: 'foo server task description - task 1',
+            done: false,
+            created: new Date()
+        },
+        {
+            description: 'bar server task description - task 2',
+            done: true,
+            created: new Date()
+        }
+    ]);
+});
+
 app.listen(3000, function() {
     console.log('listening on port 3000...');
 });
