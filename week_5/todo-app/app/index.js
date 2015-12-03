7b5fd22a614303d535b8ee179ea4fce09b476059
Robert Nations <briannations@Roberts-MacBook-Pro.local>
2015-12-03 14:40:40 -0800

    initial scss dir restructure, new updated styles, minor template updates

--- week_5/todo-app/app/index.js
+++ week_5/todo-app/app/index.js
@@ -2,6 +2,8 @@ var angular = require('angular');
 require('./controllers/controller');
 require('./filters/moment');
 
+require('./style.scss');
+
 var Todo = angular.module('Todo', [
 	'todo.list-controller',
 	'todo.moment-filter'
--- week_5/todo-app/app/webpack.config.js
+++ week_5/todo-app/app/webpack.config.js
@@ -1,36 +1,36 @@
 module.exports = {
-	entry: './index.js',
-	output: {
-		path: '../server/public',
-		filename: 'bundle.js'
-	},
-	devtool: 'source-map',
-	module: {
-		// preLoaders: [
-  //           {
-  //               test: /\.js$/, // include .js files
-  //               exclude: /node_modules/, // exclude any and all files in the node_modules folder
-  //               loader: "jshint-loader"
-  //           }
-  //       ],
-		loaders: [
-			{
-				test: /\.scss$/,
-				loader: 'style!css?sourceMap!sass?sourceMap'
-			}
-			// {
-			// 	test: /\.jsx?$/,
-			// 	exclude: /node_modules/,
-			// 	loader: 'babel',
-			//     query: {
-			//       presets: ['es2015'] //,
-			//       // cacheDirectory: true,
-			//       // plugins: ['transform-runtime']
-			//     }
-			// }
-		]
-	},
-	sassLoader: {
-	  includePaths: ['./includes']
-	}
+    entry: './index.js',
+    output: {
+        path: '../server/public',
+        filename: 'bundle.js'
+    },
+    devtool: 'source-map',
+    module: {
+        // preLoaders: [
+            // {
+                // test: /\.js$/, // include .js files
+                // exclude: /node_modules/, // exclude any and all files in the node_modules folder
+                // loader: "jshint-loader"
+            // }
+        // ],
+        loaders: [
+            {
+                test: /\.scss$/,
+                loader: 'style!css?sourceMap!sass?sourceMap'
+            }
+            // {
+                // test: /\.jsx?$/,
+                // exclude: /node_modules/,
+                // loader: 'babel',
+                // query: {
+                    // presets: ['es2015'] //,
+                    // cacheDirectory: true,
+                    // plugins: ['transform-runtime']
+                // }
+            // }
+        ]
+    },
+    sassLoader: {
+        includePaths: ['./scss/content', './scss/core', './scss/includes', './scss/layout', './scss/mobile', './scss/general']
+    }
 };
